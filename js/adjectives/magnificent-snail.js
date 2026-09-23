6c532ecb7f7286151184450aa2bd7d9c2c1aa953
marcone-correia <marcone.d.correia@gmail.com>
2026-09-23 18:15:28 +0000

    added the best adjectives ever

---
+++ js/adjectives/magnificent-snail.js
@@ -0,0 +1,15 @@
+
+
+adjectives = adjectives.concat([
+    'sparkly',
+    'chubby',
+    'exuberant',
+    'humble',
+    'perfunctory',
+    'agnostic',
+    'forked-up',
+    'juicy',
+    'half-baked',
+    'stinky',
+    'ginormous'
+]);
